7b8f38bed59dcf04a0444ee81c2fb585ea5e39e8
swen <1063642821@qq.com>
2020-01-17 15:38:31 +0800

    初始化版本

---
+++ src/main/java/com/mango/require/controller/UserController.java
@@ -0,0 +1,87 @@
+package com.mango.require.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import com.mango.require.service.IUserService;
+import com.mango.require.model.User;
+import com.mango.require.model.common.PageRequest;
+import com.mango.require.model.common.Result;
+import com.mango.require.model.common.ResultGenerator;
+import javax.annotation.Resource;
+
+/**
+ * <p>
+ * 用户信息 前端控制器
+ * </p>
+ *
+ * @author swen
+ * @since 2020-01-17
+ */
+@Api(value = "用户信息接口", tags = {"用户信息接口"})
+@Slf4j
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+     @Resource
+     private IUserService userService;
+
+     /**
+      * 用户信息列表
+      * @param user 用户信息
+      * @param pageRequest 分页参数
+      * @return Result
+      */
+     @ApiOperation(value = "用户信息列表", notes = "用户信息列表")
+     @GetMapping("/list")
+     public Result list(User user, PageRequest pageRequest) {
+        return ResultGenerator.genSuccessResult(userService.userList(user, pageRequest));
+     }
+
+     /**
+      * 用户信息新增
+      * @param user 用户信息
+      * @return Result
+      */
+     @ApiOperation(value = "用户信息新增", notes = "用户信息新增")
+     @PostMapping("/add")
+     public Result add(User user) {
+        return ResultGenerator.genSuccessResult(userService.save(user));
+     }
+
+     /**
+      * 用户信息删除
+      * @param id user主键
+      * @return Result
+      */
+     @ApiOperation(value = "用户信息删除", notes = "用户信息删除")
+     @DeleteMapping("/{id}")
+     public Result delete(@PathVariable Integer id) {
+        return ResultGenerator.genSuccessResult(userService.removeById(id));
+     }
+
+     /**
+      * 用户信息修改
+      * @param user 用户信息
+      * @return Result
+      */
+     @ApiOperation(value = "用户信息修改", notes = "用户信息修改")
+     @PostMapping("/update")
+     public Result update(User user) {
+        return ResultGenerator.genSuccessResult(userService.updateById(user));
+     }
+
+     /**
+      * 用户信息详情
+      * @param id user主键
+      * @return Result
+      */
+     @ApiOperation(value = "用户信息详情", notes = "用户信息详情")
+     @GetMapping("/{id}")
+     public Result detail(@PathVariable Integer id) {
+        return ResultGenerator.genSuccessResult(userService.getById(id));
+     }
+}
