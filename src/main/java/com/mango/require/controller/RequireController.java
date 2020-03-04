7d25e6826369ba6a40e8a64b16e1b65648d1b736
swen <1063642821@qq.com>
2020-03-04 13:53:37 +0800

    初始化版本

--- src/main/java/com/mango/require/controller/RequireController.java
+++ src/main/java/com/mango/require/controller/RequireController.java
@@ -54,7 +54,7 @@ public class RequireController {
       * @return Result
       */
      @ApiOperation(value = "需求信息列表", notes = "需求信息列表")
-     @PreAuthorize("hasAuthority('require:view')")
+     @PreAuthorize("hasAuthority('require:admin:view')")
      @GetMapping("/admin/list")
      public Result adminList(RequireAdminListCo requireAdminListCo) {
           return ResultGenerator.genSuccessResult(requireService.adminList(requireAdminListCo));
