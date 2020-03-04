04c18310649d677204940c42602efe61fdd292d5
swen <1063642821@qq.com>
2020-03-04 09:50:18 +0800

    初始化版本

--- src/main/java/com/mango/require/handler/SecurityAuthenticationProvider.java
+++ src/main/java/com/mango/require/handler/SecurityAuthenticationProvider.java
@@ -1,14 +1,9 @@
 package com.mango.require.handler;
 
-import com.alibaba.fastjson.JSONObject;
 import com.mango.require.entity.common.CurrentUser;
-import com.mango.require.entity.pojo.Dept;
-import com.mango.require.entity.pojo.Role;
-import com.mango.require.entity.pojo.User;
 import com.mango.require.service.IDeptService;
 import com.mango.require.service.IMenuService;
 import com.mango.require.service.IUserService;
-import com.mango.require.utils.MapperUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.keycloak.KeycloakSecurityContext;
@@ -21,10 +16,8 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 
 /**
  * 重写授权token赋值
@@ -43,10 +36,6 @@ public class SecurityAuthenticationProvider implements AuthenticationProvider {
     @Resource
     private IMenuService menuService;
 
-    @Resource
-    private RestTemplate restTemplate;
-
-    private static final String misUrl = "https://mis.517.cn/mangoapi/UserNoLogin/GetUserName?userid=%s";
     /**
      * 验证Authentication，建立系统使用者信息principal(token)
      */
@@ -61,39 +50,9 @@ public class SecurityAuthenticationProvider implements AuthenticationProvider {
         }
         Integer userId = Integer.valueOf(accessToken.getFamilyName());
         CurrentUser currentUser = userService.findByUserId(userId);
-        //如何空插入用户信息 默认角色2普通用户
-        if(currentUser == null){
-            JSONObject json = restTemplate.getForObject(String.format(misUrl, userId), JSONObject.class);
-            assert json != null;
-            if(json.getInteger("flag").equals(100)) {
-                JSONObject userInfo = json.getJSONObject("result");
-                User user = User.builder()
-                        .username(userInfo.getString("UserName"))
-                        .userPhoto(userInfo.getString("userphoto"))
-                        .deptId(userInfo.getInteger("OrgId"))
-                        .userId(userId)
-                        .build();
-                userService.save(user, new ArrayList<Role>() {{
-                    add(Role.builder().roleId(2).build());
-                }});
-                Dept dept = deptService.getById(user.getDeptId());
-                if(dept == null){
-                   dept = Dept.builder()
-                            .deptId(user.getDeptId())
-                            .deptName(userInfo.getString("OrgName"))
-                            .build();
-                    deptService.save(dept);
-                }
-                currentUser = MapperUtils.mapperBean(user, CurrentUser.class);
-                currentUser.setDeptName(dept.getDeptName());
-            }
-        }
-        String permissions = menuService.findUserPermissions(userId);
-        //重写token 将当前登陆人信息塞入token中
-        assert currentUser != null;
         PreAuthenticatedAuthenticationToken preAuthenticatedAuthenticationToken =
                 new PreAuthenticatedAuthenticationToken(currentUser, currentUser.getPassword(),
-                       AuthorityUtils.commaSeparatedStringToAuthorityList(permissions));
+                       AuthorityUtils.commaSeparatedStringToAuthorityList(currentUser.getPermissions()));
         preAuthenticatedAuthenticationToken.setDetails(currentUser);
         return preAuthenticatedAuthenticationToken;
     }
--- src/main/java/com/mango/require/service/impl/UserServiceImpl.java
+++ src/main/java/com/mango/require/service/impl/UserServiceImpl.java
@@ -1,20 +1,23 @@
 package com.mango.require.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.mango.require.entity.common.CurrentUser;
-import com.mango.require.entity.pojo.Role;
-import com.mango.require.entity.pojo.User;
-import com.mango.require.entity.pojo.UserRole;
+import com.mango.require.entity.pojo.*;
+import com.mango.require.exception.RequireException;
 import com.mango.require.mapper.DeptMapper;
+import com.mango.require.mapper.MenuMapper;
 import com.mango.require.mapper.UserMapper;
 import com.mango.require.mapper.UserRoleMapper;
 import com.mango.require.service.IUserService;
 import com.mango.require.utils.MapperUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -28,6 +31,10 @@ import java.util.List;
 @Transactional
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    private static final String misUrl = "https://mis.517.cn/mangoapi/UserNoLogin/GetUserName?userid=%s";
+
+    @Resource
+    private UserMapper userMapper;
 
     @Resource
     private UserRoleMapper userRoleMapper;
@@ -35,15 +42,50 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
     @Resource
     private DeptMapper deptMapper;
 
+    @Resource
+    private MenuMapper menuMapper;
+
+    @Resource
+    private RestTemplate restTemplate;
+
     @Override
     public CurrentUser findByUserId(Integer userId) {
         User user = baseMapper.selectById(userId);
+        CurrentUser currentUser;
         if(user != null) {
-            CurrentUser currentUser = MapperUtils.mapperBean(user, CurrentUser.class);
+            currentUser = MapperUtils.mapperBean(user, CurrentUser.class);
             currentUser.setDeptName(deptMapper.selectById(currentUser.getDeptId()).getDeptName());
-            return currentUser;
+        } else {
+            //如何空插入用户信息 默认角色2普通用户
+            JSONObject json = restTemplate.getForObject(String.format(misUrl, userId), JSONObject.class);
+            assert json != null;
+            if(json.getInteger("flag").equals(100)) {
+                JSONObject userInfo = json.getJSONObject("result");
+                user = User.builder()
+                        .username(userInfo.getString("UserName"))
+                        .userPhoto(userInfo.getString("userphoto"))
+                        .deptId(userInfo.getInteger("OrgId"))
+                        .userId(userId)
+                        .build();
+                userMapper.insert(user);
+                userRoleMapper.insert(UserRole.builder().roleId(2).userId(userId).build());
+                Dept dept = deptMapper.selectById(user.getDeptId());
+                if(dept == null){
+                    dept = Dept.builder()
+                            .deptId(user.getDeptId())
+                            .deptName(userInfo.getString("OrgName"))
+                            .build();
+                    deptMapper.insert(dept);
+                }
+                currentUser = MapperUtils.mapperBean(user, CurrentUser.class);
+                currentUser.setDeptName(dept.getDeptName());
+            } else {
+                throw new RequireException("获取用户信息失败");
+            }
         }
-        return null;
+        List<Menu> menus = menuMapper.getMenus(userId);
+        currentUser.setPermissions(menus.stream().map(Menu::getPerms).collect(Collectors.joining(",")));
+        return currentUser;
     }
 
     @Override
